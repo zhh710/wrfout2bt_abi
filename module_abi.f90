8fb0255a1663e1dfb987a91fa7ae556c72330549
zhh710 <zhh__710>
2021-04-24 01:48:19 -0500

    add

---
+++ module_abi.f90
@@ -0,0 +1,12 @@
+!4/24/2021 
+!
+module module_abi
+    use model_precision,only:P
+    implicit none
+    real(P),allocatable,dimension(:,:)::tb
+    real(P),allocatable,dimension(:,:)::tb_clr
+
+
+
+
+end module module_abi
